b59d05af5e152ae2ed80e8ac761a7a28bd9340b6
Harsimran singh <simran.9971290436@gmail.com>
2021-09-22 10:12:40 +0530

    add value to preview for templates

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,11 +1,38 @@
+import { useSelector } from "react-redux";
 import "./css/preview.css";
 
-let Preview = ()=>{
-    return(
-        <>
-        <div className="preview-container"></div>
-        </>
-    );
-}
+let Preview = () => {
+  let {
+    fName,
+    lName,
+    Email,
+    City,
+    state,
+    PhoneNo,
+    Degree,
+    CGPA,
+    Institute_Name,
+    year,
+  } = useSelector((state) => state.details);
+
+  let templateCode = useSelector((state) => state.template);
+
+  return (
+    <>
+      <div className="preview-container">
+        <div className={`template-${templateCode}`}>{fName}</div>
+        <div className={`template-${templateCode}`}>{lName}</div>
+        <div className={`template-${templateCode}`}>{Email}</div>
+        <div className={`template-${templateCode}`}>{City}</div>
+        <div className={`template-${templateCode}`}>{state}</div>
+        <div className={`template-${templateCode}`}>{PhoneNo}</div>
+        <div className={`template-${templateCode}`}>{Degree}</div>
+        <div className={`template-${templateCode}`}>{CGPA}</div>
+        <div className={`template-${templateCode}`}>{Institute_Name}</div>
+        <div className={`template-${templateCode}`}>{year}</div>
+      </div>
+    </>
+  );
+};
 
 export default Preview;
